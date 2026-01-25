52f8fd7322130a3351fcdb99486c4e76cc60b95a
Yehia Ayman <yhyaayman.ya@gmail.com>
2026-01-25 21:23:54 +0200

    Best_Blink_Ever.ino, adding serialSplash

--- Best_Blink_Ever.ino
+++ Best_Blink_Ever.ino
@@ -1,3 +1,18 @@
+#define PROG_NAME "Best_Blink_Ever"
+// Author: Yehia Ayman
+// Date: 20260125
+// A simple code on which to develope features.
+
+// Customized this by changing these defines
+
+#define VERSION " V0.0.1 "
+#define MODEL_NAME "Model: ESP32 Dev Module"
+#define DEVICE_UNDER_TEST "SN: 00001"  //A Serial Number
+#define LICENSE "GNU Affero General Public License, version 3 "
+#define ORIGIN "EG"
+
+#define BAUDRATE 115200  //Serial port
+
 /*
   Blink
 
@@ -21,16 +36,39 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
+
+
 #define LED_BUILTIN 2
+
+void serialSplash(void) {
+  //Serial splash
+  Serial.println(F("==================================="));
+  Serial.print(PROG_NAME);
+  Serial.println(VERSION);
+  Serial.println(MODEL_NAME);
+  Serial.println(DEVICE_UNDER_TEST);
+  Serial.print(F("Compiled at: "));
+  Serial.println(F(__DATE__ " " __TIME__));  //compile date that is used for a unique identifier
+  Serial.println(LICENSE);
+  Serial.println(F("==================================="));
+  Serial.println();
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(BAUDRATE);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB
+  }
   pinMode(LED_BUILTIN, OUTPUT);
+  serialSplash();
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  
   delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
