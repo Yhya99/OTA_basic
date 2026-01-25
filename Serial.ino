b1ab24a150a9e737b1210d1f18b25fda2bb8de1e
Yehia Ayman <yhyaayman.ya@gmail.com>
2026-01-25 21:32:47 +0200

    Serial.ino, added a tab and moved serialSplash into the tab V0.0.2

---
+++ Serial.ino
@@ -0,0 +1,13 @@
+void serialSplash(void) {
+  //Serial splash
+  Serial.println(F("==================================="));
+  Serial.print(PROG_NAME);
+  Serial.println(VERSION);
+  Serial.println(MODEL_NAME);
+  Serial.println(DEVICE_UNDER_TEST);
+  Serial.print(F("Compiled at: "));
+  Serial.println(F(__DATE__ " " __TIME__));  //compile date that is used for a unique identifier
+  Serial.println(LICENSE);
+  Serial.println(F("==================================="));
+  Serial.println();
+}
