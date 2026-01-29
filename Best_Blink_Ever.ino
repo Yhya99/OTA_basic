6835f86518af50334a037a165736adce7b62e5da
Yehia Ayman <yhyaayman.ya@gmail.com>
2026-01-29 22:44:52 +0200

    Best_Blink_Ever.ino, Adding the OTA and adding a page route /yehia V0.0.4

--- Best_Blink_Ever.ino
+++ Best_Blink_Ever.ino
@@ -1,18 +1,39 @@
 #define PROG_NAME "Best_Blink_Ever"
-#define VERSION " V0.0.3 "
+#define VERSION " V0.0.4 "
 // Author: Yehia Ayman
 // Date: 20260125
 // A simple code on which to develope features.
 
 // Customized this by changing these defines
 
-
 #define MODEL_NAME "Model: ESP32 Dev Module"
 #define DEVICE_UNDER_TEST "SN: 00001"  //A Serial Number
 #define LICENSE "GNU Affero General Public License, version 3 "
 #define ORIGIN "EG"
 
+const char* ssid = "VRX";
+const char* password = "textinsert";
+
+// const char* ssid = "WE_NET";
+// const char* password = "AymanSH@2025_**";
+
+// const char* ssid = "Galaxy A53 5GD5E1";
+// const char* password = "1234567888*";
+
 #define BAUDRATE 115200  //Serial port
+
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+#elif defined(TARGET_RP2040) || defined(TARGET_RP2350) || defined(PICO_RP2040) || defined(PICO_RP2350)
+  #include <WiFi.h>
+#endif
+
+#include <ESPAsyncWebServer.h>
+#include <ElegantOTA.h>
+
+
 
 /*
   Blink
@@ -83,6 +104,35 @@ public:
 };
 
 Flasher led1(LED_BUILTIN, 250, 250);  // LED 2 blinks 0.5 period and 50% duty cycle
+
+AsyncWebServer server(80);
+
+unsigned long ota_progress_millis = 0;
+
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -92,11 +142,45 @@ void setup() {
   }
   pinMode(LED_BUILTIN, OUTPUT);
   serialSplash();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! This is ElegantOTA AsyncDemo.");
+  });
+  server.on("/yehia", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! This is Yehia.");
+  });
+
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  // ElegantOTA callbacks
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
-led1.Update();
+  ElegantOTA.loop();
+  led1.Update();
   // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   
   // delay(1000);                      // wait for a second
